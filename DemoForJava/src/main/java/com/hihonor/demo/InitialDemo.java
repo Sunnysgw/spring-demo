f54f431cc3ae1b5ed8e799bc798653318c6aa344
sunnysgw <sgw153759@163.com>
2022-09-25 18:26:54 +0800

    gc demo

---
+++ DemoForJava/src/main/java/com/hihonor/demo/InitialDemo.java
@@ -0,0 +1,29 @@
+package com.hihonor.demo;
+
+public class InitialDemo {
+
+    int a;
+
+    short b;
+
+    long c;
+
+    float d;
+
+    double e;
+
+    boolean f;
+
+    byte g;
+
+    char h;
+
+    public InitialDemo() {
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+        new InitialDemo();
+    }
+
+}
--- DemoForJava/src/main/java/com/hihonor/demo/StartOrder.java
+++ DemoForJava/src/main/java/com/hihonor/demo/StartOrder.java
@@ -8,19 +8,34 @@ public class StartOrder {
     String b = "world";
 
     static {
-        System.out.println("静态代码块");
+        System.out.println("father静态代码块");
     }
 
     {
-        System.out.println("构造代码块");
+        System.out.println("father构造代码块");
     }
 
     public StartOrder() {
-        System.out.println("构造方法");
+        System.out.println("father构造方法");
+    }
+
+    private static class Child extends StartOrder {
+        static {
+            System.out.println("child静态代码块");
+        }
+
+        {
+            System.out.println("child构造代码块");
+        }
+
+        public Child() {
+            System.out.println("child构造方法");
+        }
+
     }
 
     public static void main(String[] args) {
-        StartOrder demo = new StartOrder();
+        new Child();
     }
 
 }
